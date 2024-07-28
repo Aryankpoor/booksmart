1d3c401917acc2c586d08459b905a1083e5c7c2a
Aryankpoor <Kapooraryan0419@gmail.com>
2024-07-28 22:37:00 +0530

    added routes using browser router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./Components/Navbar/Navbar";
 import { BrowserRouter,Routes,Route  } from 'react-router-dom';
 import Shop from './Pages/Shop'
 
+
 function App() {
   return (
     <div>
